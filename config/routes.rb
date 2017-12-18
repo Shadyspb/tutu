237a42936e9c4d5f74c5a2d8f36cd0dd7efcb695
shady <Shad1k@myttk.ru>
2017-12-18 21:02:14 +0300

    tutu lesson_14_исправления

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
   resources :routes
   resources :tickets
   resources :users
-  resources :railway_stations_route
   get 'welcome/index'
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
